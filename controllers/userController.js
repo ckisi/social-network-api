2967994a97bae07cd14887e11dd3a822720243cd
Lain <ckisielius@gmail.com>
2024-09-30 11:06:57 -0400

    add update and delete user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,9 @@ module.exports = {
   // get a single user
   async getSingleUser(req, res) {
     try {
-      const user = await User.findOne({ id: req.params.userId }).select("-__v");
+      const user = await User.findOne({ _id: req.params.userId }).select(
+        "-__v"
+      );
 
       if (!user) {
         return res.status(404).json({ message: "No user with that ID" });
@@ -38,4 +40,39 @@ module.exports = {
     }
   },
   // update a user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "No application with this id!" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  // delete a user
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+
+      res.json({ message: "User successfully deleted" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const { getUsers, getSingleUser, createUser } = require("../../controllers/userController");
+const { getUsers, getSingleUser, createUser, updateUser, deleteUser } = require("../../controllers/userController");
 
 // api/users
 router.route("/").get(getUsers).post(createUser);
 
-router.route("/:userId").get(getSingleUser);
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
